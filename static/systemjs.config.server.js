5a3e98efa1f9b71b12532d13a21a02d10139fc46
undefeat <eridan.maksim@gmail.com>
2017-02-06 10:08:36 +0200

    sa

--- static/systemjs.config.server.js
+++ static/systemjs.config.server.js
@@ -6,7 +6,7 @@
 (function (global) {
   System.config({
     paths: {
-      'npm:': 'node_modules/' // path serves as alias
+      'npm:': 'https://unpkg.com/' // path serves as alias
     },
     // map tells the System loader where to look for things
     map: {
@@ -26,7 +26,7 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'crypto-js':                 'npm:crypto-js'
+      'crypto-js':                 'node_modules/crypto-js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
